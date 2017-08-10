66790f5aabf57822477024fbb7fb1b8e56519c74
adityasingh1993 <aditya.kumar@aot.edu.in>
2017-08-11 00:20:21 +0530

    Update simple_linear_regression.py
    Simple Linear Regression Code in Python

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,47 +1,72 @@
-# Simple Linear Regression
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 22:02:37 2017
 
-# Importing the libraries
+@author: Aditya
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+#Loading Data
+dataset=pd.read_csv('Salary_Data.csv')
+print (dataset)
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,1].values
 
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+            
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------             
+#Missing value data insertion, since our datasheet does'nt have any missing values we wont do it.
+#But we will keep the code in comment
 
-# Splitting the dataset into the Training set and Test set
+
+
+#from sklearn.preprocessing import Imputer
+#imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)  
+#X=imputer.fit_transform(X[:,0]) 
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#As we know encoding is required when we caterogical data, So here we dont require encoding, but we will write code
+
+
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#labelEncoder_X=LabelEncoder()
+#X[:,0]=labelEncoder_X.fit_transform(X[:,0])
+#oneHotEncoder_X=OneHotEncoder(caterogical_features=[0])
+#X=oneHotEncoder_X.fit_transform(X).toarray()
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Splitting The Data
+
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
-# Feature Scaling
+#Feature Selection
 """from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X)
+X_test=sc_X.transform(X)"""
 
-# Fitting Simple Linear Regression to the Training set
+                     
+#Simple Linear Regression
+
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
+y_pred=regressor.predict(X_test)
+#print(y_pred)
+My_pred=regressor.predict(40)
+
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(ONTrain)')
+plt.xlabel('Years Of Exp')
 plt.ylabel('Salary')
 plt.show()
-
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+                           
